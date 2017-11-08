ccb4029baa251c70ae7b9cff21cb498c69271c37
Jim Stricker <jmstricker93@gmail.com>
2017-11-08 10:36:20 -0500

    added validations for Resource, User.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,9 @@
 class Resource < ApplicationRecord
+
+  validates :title => uniqueness: {case_sensitive: false}
+  validates :source_url => presence: true
+
+
   belongs_to :car
   belongs_to :resource_type
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
+  validates :name => presence: true
+
   has_many :user_cars
   has_many :cars, through: :user_cars
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
-  
