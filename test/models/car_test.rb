630b49702dd0bb619bc5c89c1f382f126699913a
Jim Stricker <jmstricker93@gmail.com>
2017-11-08 15:24:29 -0500

    added unit tests for Car model

--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -5,9 +5,18 @@ class CarTest < ActiveSupport::TestCase
   test "should not be valid without name" do
     sedan = CarType.find_by(name: "Sedan")
     honda = Brand.find_by(name: "Honda")
+
     car = Car.new(brand: honda, year: 1990)
     car.car_types << sedan
     assert_not car.save, "saved without name"
+  end
+
+  test "should not be valid without brand" do
+    sedan = CarType.find_by(name: "Sedan")
+
+    car = Car.new(name: "Accord", year: 1990)
+    car.car_types << sedan
+    assert_not car.save, "saved without brand"
   end
 
 end
