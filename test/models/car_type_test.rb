e7abb2f265885b5e662e4094a8bad934b829374c
Jim Stricker <jmstricker93@gmail.com>
2017-11-08 15:27:34 -0500

    added unit tests for CarType model

--- test/models/car_type_test.rb
+++ test/models/car_type_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CarTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save without a name" do
+    car_type = CarType.new()
+    assert_not car_type.save, "Saved without a name"
+  end
+
 end
