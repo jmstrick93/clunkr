8931ed76bdb20698520219672fdb85481f433106
Jim Stricker <jmstricker93@gmail.com>
2017-11-09 09:43:13 -0500

    setup some more model tests, but encountered difficulties when working with User devise model - decided to abandon

--- test/models/brand_test.rb
+++ test/models/brand_test.rb
@@ -2,17 +2,25 @@ require 'test_helper'
 
 class BrandTest < ActiveSupport::TestCase
 
+  def setup
+    @brand = brands(:mg)
+  end
+
   #validations
 
+  test "fixture is valid" do
+    assert @brand.save
+  end
+
   test "should not save without a name" do
-    brand = Brand.new()
-    assert_not brand.save, "Saved without a name"
+    @brand.name = nil
+    assert_not @brand.save, "Saved without a name"
   end
 
   #associations
 
   test "Has many cars" do
-
+    assert_equal 2, @brand.cars.count
   end
 
 end
--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -2,37 +2,39 @@ require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
 
+  def setup
+    @car = cars(:accord)
+    @sedan = car_types(:sedan)
+    @coupe = car_types(:coupe)
+  end
+
   #validations
 
   test "should not be valid without name" do
-    sedan = CarType.find_by(name: "Sedan")
-    honda = Brand.find_by(name: "Honda")
-
-    car = Car.new(brand: honda, year: 1990)
-    car.car_types << sedan
-    assert_not car.save, "saved without name"
+    @car.name = nil
+    assert_not @car.save, "saved without name"
   end
 
   test "should not be valid without brand" do
-    sedan = CarType.find_by(name: "Sedan")
-
-    car = Car.new(name: "Accord", year: 1990)
-    car.car_types << sedan
-    assert_not car.save, "saved without brand"
+    @car.brand = nil
+    assert_not @car.save, "saved without brand"
   end
 
   #associations
 
   test "belongs to brand" do
-
+    assert_instance_of Brand, @car.brand, "#{@car.brand} is not an instance of Brand"
   end
 
   test "has many car types" do
-
+    assert_equal 2, @car.car_types.count
+    @car.car_types.each do |t|
+      assert_instance_of CarType, t, "#{t} is not an instance of CarType"
+    end
   end
 
   test "has many users" do
-    
+
   end
 
 end
