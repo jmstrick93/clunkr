80010b93ba2036a82257a6f1e1babfb5a8f9f75c
Jim Stricker <jmstricker93@gmail.com>
2017-11-09 18:08:28 -0500

    users devise removed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :cars do
     resources :resources, only: [:show, :index]
   end
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#root'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109221200) do
+ActiveRecord::Schema.define(version: 20171109230549) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
@@ -56,24 +56,6 @@ ActiveRecord::Schema.define(version: 20171109221200) do
     t.string "color"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
