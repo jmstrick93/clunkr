d92865a0c7a5cb13f4874d4589b12a094bcf9490
Jim Stricker <jmstricker93@gmail.com>
2017-11-09 17:39:31 -0500

    commented out name validation due to complications with Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  validates :name, presence: true, uniqueness: true
+  # removed_the_below due to complications with Devise.
+  # will instead allow a user to edit their alias in their user-view page
+  
+  # validates :name, presence: true, uniqueness: true
 
   has_many :user_cars
   has_many :cars, through: :user_cars
