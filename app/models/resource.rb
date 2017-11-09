b6ccaf2622e5c0fceeb3ac79b6d4c1909c50a1c0
Jim Stricker <jmstricker93@gmail.com>
2017-11-08 21:09:01 -0500

    made some corrections to basic model tests and association macros

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,9 +1,9 @@
 class Resource < ApplicationRecord
 
-  validates :title, uniqueness: {case_sensitive: false}
+  validates :title, presence: true, uniqueness: {case_sensitive: false}
   validates :source_url, presence: true
   validates :resource_type, presence: true
 
-  belongs_to :car
+  belongs_to :car, optional: true
   belongs_to :resource_type
 end
