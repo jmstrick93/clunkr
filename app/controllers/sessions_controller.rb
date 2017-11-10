53a17e04566aa5670f929ffdb4a98cbdf5993df5
Jim Stricker <jmstricker93@gmail.com>
2017-11-10 16:19:32 -0500

    wrapped some SessionsController logic in helper methods and fixed login bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,22 +6,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #TOO FAT! SLIM DOWN LATER
+    #FAT CONTROLLER! SLIM DOWN LATER
     if auth
       @user = User.find_or_create_by(uid: auth['uid']) do |u|
         u.username = auth['info']['name']
         u.email = auth['info']['email']
         u.password = SecureRandom.urlsafe_base64(n=6)
       end
-      if @user.errors
+      #wrap the below in HandleOmniAuthErrors in User model
+      if !@user.errors.empty?
         binding.pry
         if @user.errors.keys.include?(:email)
-          flash[:error] ||= []
-          flash[:error] << "A user has already registered this Google email address with a non-Google account.  Please log in to this account without Google."
+          omniauth_email_error
           return redirect_to sign_in_path
         else
-          flash[:error] ||= []
-          flash[:error] ||= "An unexpected error with Google SignIn has occurred. We are working to fix this as soon as we can."
+          general_omniauth_error
+          return redirect_to sign_in_path
         end
       end
       session[:user_id] = @user.id
@@ -60,6 +60,16 @@ class SessionsController < ApplicationController
 
   def auth
     request.env['omniauth.auth']
+  end
+
+  def omniauth_email_error
+    flash[:error] ||= []
+    flash[:error] << "A user has already registered this Google email address with a non-Google account.  Please log in to this account without using Google."
+  end
+
+  def general_omniauth_error
+    flash[:error] ||= []
+    flash[:error] << "An unexpected error with Google SignIn has occurred. We are working to fix this as soon as we can."
   end
 
 end
