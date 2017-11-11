43cf936b4a1987b0f1a6282c8737d204f766b1e4
Jim Stricker <jmstricker93@gmail.com>
2017-11-11 17:51:02 -0500

    Got all cars nested forms working

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -19,14 +19,20 @@ class CarsController < ApplicationController
 
   def create
     @car = Car.new(car_params)
+    if !@car.brand.valid?
+      @car.brand = Brand.find_by(id: car_params[:brand_id])
+    else
+      @car.brand.save
+      @car.brand_id = @car.brand.id
+    end
     @car.car_types = @car.car_types.reject {|type| type.id.blank?}
-    @car.brand.save
-    @car.brand_id = @car.brand.id
+    binding.pry
     if @car.save
       binding.pry
       redirect_to car_path(@car)
     else
-      flash[:alert] = "#{error_count(@car)} errors prevented this car from saving: "
+      flash[:alert] = view_context.pluralize(@car.errors.count,
+      'error')+ " prevented this car from saving: "
       prep_flash_errors(@car)
       redirect_to new_car_path
     end
