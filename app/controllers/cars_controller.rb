0de47363a9f1cf4ae1beb0e64277409e9af762a8
Jim Stricker <jmstricker93@gmail.com>
2017-11-11 11:47:46 -0500

    Mostly set up new-car form (and nested forms), functionality not yet tested.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -10,5 +10,14 @@ class CarsController < ApplicationController
     @car = Car.find_by(id: params[:id])
   end
 
+  def new
+    @car = Car.new
+    @brand = Brand.new
+    @car_type = CarType.new
+  end
+
+  def create
+  end
+
 
 end
