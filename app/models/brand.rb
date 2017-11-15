feb5314e2181e9fabaf248fe715fc79d4975b7a4
Jim Stricker <jmstricker93@gmail.com>
2017-11-15 14:45:02 -0500

    added dependent: :destroy to brand

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,7 +3,7 @@ class Brand < ApplicationRecord
   validates :name, presence: true, uniqueness: {case_sensitive: false}
   validate :brand_photo_url_is_valid
 
-  has_many :cars
+  has_many :cars, dependent: :destroy
   has_many :car_types, through: :cars
 
   # accepts_nested_attributes_for :cars
@@ -14,7 +14,7 @@ class Brand < ApplicationRecord
   def brand_photo_url_is_valid
     if self.logo =~ /\A#{URI::regexp}[.](jpg|JPG|jpeg|JPEG|gif|GIF|png|PNG)\z/
       uri = URI.parse(self.logo)
-      
+
       unless !uri.host.nil?
         errors.add(:logo, "must be valid url to jpeg/jpg, gif, or png file.")
       end
