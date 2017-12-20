6f172bacc5d5245936c6b1825a641d5964e288db
Jim Stricker <jmstricker93@gmail.com>
2017-12-20 14:31:42 -0500

    cars index initial load now renders properly using AJAX/javascript

--- app/assets/javascripts/cars/car_index.js
+++ app/assets/javascripts/cars/car_index.js
@@ -1,9 +1,16 @@
 document.addEventListener("turbolinks:load", function(){
   const $carListDiv =  $("body.index.cars div#car-index-list")
-  debugger;
-  $carListDiv.html(`<div>
-  <a href="/cars/1"><img src="https://upload.wikimedia.org/wikipedia/commons/1/11/1965_Chevrolet_Impala_Super_Sport_Coupe_%281%29.JPG" alt="1965 chevrolet impala super sport coupe %281%29" width="300" height="200" /></a>
-  <p><a href="/cars/1">1493 Chevrolet Impalaaa</a></p>
-  <br></br>
-</div>`)
+
+  let newCarListDivContents = ""
+
+  $.get("/cars.json", function(response){
+    for (let c of response){
+      newCarListDivContents += `<div>
+      <a href="/cars/${c.id}"><img src="${c.photo_url}" width="300" height="200" /></a>
+      <p><a href="/cars/${c.id}">${c.year} ${c.brand.id} ${c.name}</a></p>
+      <br></br>
+    </div>`
+    }
+    $carListDiv.html(newCarListDivContents)
+  })
 })
