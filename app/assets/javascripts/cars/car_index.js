6ad557596b87e6c0aa7d9a4ae9b2378f61227a28
Jim Stricker <jmstricker93@gmail.com>
2017-12-20 14:12:24 -0500

    stub car index element being added via javascript

--- app/assets/javascripts/cars/car_index.js
+++ app/assets/javascripts/cars/car_index.js
@@ -1,5 +1,9 @@
 document.addEventListener("turbolinks:load", function(){
-
-  
-
+  const $carListDiv =  $("body.index.cars div#car-index-list")
+  debugger;
+  $carListDiv.html(`<div>
+  <a href="/cars/1"><img src="https://upload.wikimedia.org/wikipedia/commons/1/11/1965_Chevrolet_Impala_Super_Sport_Coupe_%281%29.JPG" alt="1965 chevrolet impala super sport coupe %281%29" width="300" height="200" /></a>
+  <p><a href="/cars/1">1493 Chevrolet Impalaaa</a></p>
+  <br></br>
+</div>`)
 })
