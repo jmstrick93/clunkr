4192c881eba2b30ba57f6a25eef9da3dcbff56f6
Jim Stricker <jmstricker93@gmail.com>
2017-12-20 15:41:33 -0500

    slight refactoring of cars-index-js

--- app/assets/javascripts/cars/car_index.js
+++ app/assets/javascripts/cars/car_index.js
@@ -3,29 +3,28 @@ document.addEventListener("turbolinks:load", function(){
   const $filterButton = $("body.index.cars input.filter-submit")
   const $filterForm = $("body.index.cars form.filter-form")
 
+  loadCarsIndexAjax($.get("/cars.json"))
+
   $filterForm.submit(function(e){
     e.preventDefault()
     let values = $(this).serialize()
     let carList = $.get('/cars.json', values)
     loadCarsIndexAjax(carList)
   })
-  loadCarsIndexAjax($.get("/cars.json"))
+
+
+  function loadCarsIndexAjax(getRequest){
+    let newCarListDivContents = ""
+    getRequest.done(function(response){
+      for (let c of response){
+        newCarListDivContents += `<div>
+        <a href="/cars/${c.id}"><img src="${c.photo_url}" width="300" height="200" /></a>
+        <p><a href="/cars/${c.id}">${c.year} ${c.brand.name} ${c.name}</a></p>
+        <br></br>
+      </div>`
+      }
+      $carListDiv.html(newCarListDivContents)
+    })
+  }
+
 })
-
-
-function loadCarsIndexAjax(getRequest){
-  const $carListDiv =  $("body.index.cars div#car-index-list")
-
-  let newCarListDivContents = ""
-
-  getRequest.done(function(response){
-    for (let c of response){
-      newCarListDivContents += `<div>
-      <a href="/cars/${c.id}"><img src="${c.photo_url}" width="300" height="200" /></a>
-      <p><a href="/cars/${c.id}">${c.year} ${c.brand.name} ${c.name}</a></p>
-      <br></br>
-    </div>`
-    }
-    $carListDiv.html(newCarListDivContents)
-  })
-}
